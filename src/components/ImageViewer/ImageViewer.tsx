ebefb757e5bae263a95f6988859c64adc981d390
Comum <miguel.rib.20@gmail.com>
2021-03-07 11:14:40 +0000

    feat: add finite slider

--- src/components/ImageViewer/ImageViewer.tsx
+++ src/components/ImageViewer/ImageViewer.tsx
@@ -31,7 +31,11 @@ const ImageViewer: React.FC<ImageViewerProps> = (props) => {
   const { currentImageIndex = 0, children, ...remainingProps } = props;
 
   return (
-    <div className="image-viewer" {...remainingProps}>
+    <div
+      className="image-viewer"
+      style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
+      {...remainingProps}
+    >
       {children}
     </div>
   );
