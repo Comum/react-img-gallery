30c102d94db61c4e052ce8572c3ce2eaccb77e5c
Comum <miguel.rib.20@gmail.com>
2021-03-07 10:16:47 +0000

    feat: add increment and decrement

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+
+import "./Button.scss";
+
+interface ButtonProps {
+  className?: string;
+
+  /**
+   * Orientation of the arrow.
+   *
+   * @default left
+   */
+  orientation: "right" | "left";
+
+  /**
+   * Determines if the button is disabled.
+   *
+   * @default false
+   */
+  isDisabled?: boolean;
+
+  /**
+   * Fucntion to run when the arrow is clicked
+   */
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    orientation = "left",
+    isDisabled = false,
+    className,
+    onClick,
+    ...remainingProps
+  } = props;
+  const buttonClassList = `button ${className ? className : ""}`;
+  const arrowClassList = `arrow arrow-${orientation}`;
+
+  return (
+    <button
+      className={buttonClassList}
+      disabled={isDisabled}
+      onClick={onClick}
+      {...remainingProps}
+    >
+      <div className={arrowClassList}></div>
+    </button>
+  );
+};
+
+export { Button };
--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,79 @@
 import * as React from "react";
 import "./slider.scss";
 
-import { Arrow } from "./components/Arrow";
+import { Button } from "./components/Button";
 
-const Slider: React.FC = () => {
+interface SliderProps {
+  className?: string;
+
+  /**
+   * Controls wheter the slider is finite or not.
+   * Ie. When the user reaches the end of the image list ( begining or end ) does it reset to the other side.
+   *
+   * @default true
+   */
+  isInfinite?: boolean;
+
+  /**
+   * List of images to be displayed. Expects a list of urls to be used in a <img /> element.
+   */
+  imageList: string[];
+}
+
+const Slider: React.FC<SliderProps> = (props) => {
+  const { isInfinite = true, imageList, ...remainingProps } = props;
   const [currentImage, setCurrentImage] = React.useState(0);
+  const [incrementDisabled, setIncrementDisabled] = React.useState(
+    isInfinite && imageList.length <= 1
+  );
+  const [decrementDisabled, setDecrementDisabled] = React.useState(!isInfinite);
 
   const incrementImage = () => {
-    setCurrentImage(currentImage + 1);
+    let newValue = currentImage + 1;
+
+    if (!isInfinite) {
+      setIncrementDisabled(currentImage + 1 === imageList.length - 1);
+    } else {
+      newValue = currentImage === imageList.length - 1 ? 0 : currentImage + 1;
+    }
+
+    if (decrementDisabled) {
+      setDecrementDisabled(false);
+    }
+    setCurrentImage(newValue);
   };
 
   const decrementImage = () => {
-    setCurrentImage(currentImage - 1);
+    let newValue = currentImage - 1;
+
+    if (!isInfinite) {
+      setDecrementDisabled(currentImage - 1 === 0);
+    } else {
+      newValue = currentImage === 0 ? imageList.length - 1 : currentImage - 1;
+    }
+
+    if (incrementDisabled) {
+      setIncrementDisabled(false);
+    }
+    setCurrentImage(newValue);
   };
 
   return (
-    <div className="slider">
+    <div className="slider" {...remainingProps}>
       <div className="button-container button-container-left">
-        <Arrow orientation="left" onClick={decrementImage} />
+        <Button
+          orientation="left"
+          isDisabled={decrementDisabled}
+          onClick={decrementImage}
+        />
       </div>
-      <div className="slider-content"></div>
+      <div className="slider-content">{imageList[currentImage]}</div>
       <div className="button-container button-container-right">
-        <Arrow orientation="right" onClick={incrementImage} />
+        <Button
+          orientation="right"
+          isDisabled={incrementDisabled}
+          onClick={incrementImage}
+        />
       </div>
     </div>
   );
